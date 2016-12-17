f649f3d9aa7bfe7135e9baafb95162e8dec5b753
Zach Chandler <chandler@Zachs-MacBook-Pro.local>
2016-12-17 09:36:38 -0600

    manifest update

--- app/src/main/java/com/madmensoftware/www/pops/Fragments/PopperCurrentJobsFragment.java
+++ app/src/main/java/com/madmensoftware/www/pops/Fragments/PopperCurrentJobsFragment.java
@@ -75,6 +75,7 @@ public class PopperCurrentJobsFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
+
         Query jobQuery = mRef.child("job-poppers").child(auth.getCurrentUser().getUid()).orderByChild("status").equalTo("active");
         mJobAdapter = new PopperJobAdapter(Job.class, R.layout.job_list_row, PopperJobViewHolder.class, jobQuery, getContext());
 
